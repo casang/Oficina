7cfcf584180effc0bdd46a5c1910279c05c0e4aa
casang <carlos.sang@gmail.com>
2020-02-04 15:16:38 -0300

    Update Dimmer.h

--- Dimmer.h
+++ Dimmer.h
@@ -7,12 +7,15 @@
 
 #ifdef SecaoDimmer  
 
+// 
+// canal 0 é utilizado para comandar os outros n canais
+// não possui dispositivo fisico associado
+//
 #define DIMMER_CHANNELS 5
 
 class Dimmer {
   int pinAcLoad[DIMMER_CHANNELS]; //ignite the TRIAC
   int pinZeroCross;
-  bool bOn[DIMMER_CHANNELS];
 
   volatile int zeroCrossOn[DIMMER_CHANNELS];
   volatile int dimtimeSet[DIMMER_CHANNELS];
@@ -23,10 +26,8 @@ class Dimmer {
 public:
   void acLoadInterrupt();
   void zeroCrossInterrupt();  // function to be fired at the zero crossing to dim the light
-  void turnOff ();
-  void setIntensity (int channel, int intensity, bool setOn = true); 
+  void setIntensity (int channel, int intensity); 
   Dimmer (int pinZC, int pinAcL[], void (*zcIsr)(void), void (*acLIsr) (void), int dimt[] = NULL);
-  bool isOn (int channel);
 };
 
 #endif
