9b75b7784d43e89f29692e5c830705bf5ec79578
casang <carlos.sang@gmail.com>
2020-05-12 09:33:29 -0300

    Update LevelSensor.h

--- LevelSensor.h
+++ LevelSensor.h
@@ -1,3 +1,6 @@
+#ifndef LEVEL_SENSOR_H
+#define LEVEL_SENSOR_H
+
 class LevelSensor
 {
 private:
@@ -9,3 +12,5 @@ public:
 	LevelSensor (bool na, int pinL, int pinH);
 	int level (); // 0 - abaixo do Low, 1 - entre Low e High e 2 - acima do High
 };
+
+#endif
